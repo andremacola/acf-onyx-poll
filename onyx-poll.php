4c2acfc051ebdb6fbcfb8819d5fffa667ceb21ae
Andre Macola <andremacola@gmail.com>
2020-04-20 23:59:51 -0300

    plugin acf adjustments

--- onyx-poll.php
+++ onyx-poll.php
@@ -39,7 +39,6 @@ Class OnyxPollsInit {
 	 */
 	public function add_footer_elements() {
 		if ($poll = OnyxPolls::has_polls(true)) {
-			echo do_shortcode('[onyx-poll id="38"]');
 			echo "<div id='onyx-poll-modal' class='onyx-poll onyx-poll-modal active' data-poll='$poll'></div>";
 			echo "<script>var onyxPollModal = true;</script>";
 		}
@@ -114,9 +113,9 @@ Class OnyxPollsInit {
 	 */
 	function initialize() {
 		// Change ACF Local JSON save location to /acf folder inside this plugin
-		add_filter('acf/settings/save_json', function() {
-			return __DIR__ . '/acf';
-		});
+		// add_filter('acf/settings/save_json', function() {
+		// 	return __DIR__ . '/acf';
+		// });
 
 		// Include the /acf folder in the places to look for ACF Local JSON files
 		add_filter('acf/settings/load_json', function($paths) {
