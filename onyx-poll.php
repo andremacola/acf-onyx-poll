e0b42e2301e3567e6d6d471806e518f9a57e5860
Andre Macola <andremacola@gmail.com>
2021-01-13 23:22:19 -0300

    fix for #https://github.com/andremacola/acf-onyx-poll/issues/12

--- onyx-poll.php
+++ onyx-poll.php
@@ -88,9 +88,9 @@ Class OnyxPollsInit {
 	 */
 	 public function initialize() {
 		// Change ACF Local JSON save location to /acf folder inside this plugin
-		add_filter('acf/settings/save_json', function() {
-			return __DIR__ . '/acf';
-		});
+		// add_filter('acf/settings/save_json', function() {
+		// 	return __DIR__ . '/acf';
+		// });
 
 		// Include the /acf folder in the places to look for ACF Local JSON files
 		// add_filter('acf/settings/load_json', function($paths) {
